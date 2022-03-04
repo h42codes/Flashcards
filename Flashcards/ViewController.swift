56a3338a483816dc107227e0f94ecbfdb3bab513
h42codes <99951343+h42codes@users.noreply.github.com>
2022-03-05 02:35:46 +0900

    Finished Lab 1 Optional 2: Styling the card

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,14 +11,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // outer container (card) with rounded corners and shadow
+        card.layer.cornerRadius = 20.0
+        
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.5
+        
+        // front/back labels with rounded corners
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
     }
     
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         frontLabel.isHidden.toggle()
     }
-    
 }
